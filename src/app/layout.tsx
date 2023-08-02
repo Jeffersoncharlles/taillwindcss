f4d68d291a4a72d7e32ef7a4a85a1f61859c6381
Jefferson Charlles <contato@jefferdeveloper.com>
2023-08-02 10:44:48 -0300

    chore:start layout dashboard dark and light

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { SideBar } from '@/components/Sidebar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+  description: 'Create dashboard with tailwindcss',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <div className="grid min-h-screen grid-cols-app dark:bg-zinc-900 dark:text-zinc-100 ">
+          <SideBar />
+          <main className="px-4 pb-12 pt-8">{children}</main>
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem,20rem) 1fr',
+      },
+    },
+  },
+  plugins: [],
+}
