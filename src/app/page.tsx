dfac468cf55b65d96f0d672fac66537b06519c07
Jefferson Charlles <contato@jefferdeveloper.com>
2023-08-05 09:49:08 -0300

    chore:basic form struture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,6 +73,88 @@ export default function Home() {
               />
             </Input.Root>
           </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
+              Your photo
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500 dark:text-zinc-400">
+                This will be displayed on your profile
+              </span>
+            </label>
+            <div className=""></div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="role"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
+              Role
+            </label>
+            <div className="grid grid-cols-2 gap-6">
+              <Input.Root>
+                <Input.Control id="role" defaultValue="CTO" />
+              </Input.Root>
+            </div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="country"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
+              Country
+            </label>
+            <div className=""></div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="timezone"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
+              Timezone
+            </label>
+            <div className=""></div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="bio"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
+              Bio
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500 dark:text-zinc-400">
+                Write a short introduction.
+              </span>
+            </label>
+            <div className=""></div>
+          </div>
+          <div className="grid grid-cols-form gap-3 pt-5">
+            <label
+              htmlFor="projects"
+              className="text-sm font-medium text-zinc-700 dark:text-zinc-300"
+            >
+              Portfolio projects
+              <span className="mt-0.5 block text-sm font-normal text-zinc-500 dark:text-zinc-400">
+                Share a few snippets of your work.
+              </span>
+            </label>
+            <div className=""></div>
+          </div>
+
+          <div className="flex items-center justify-end gap-2 pt-5">
+            <button
+              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50 dark:border-zinc-400 dark:text-zinc-300 dark:hover:bg-zinc-800"
+              type="button"
+            >
+              Cancel
+            </button>
+            <button
+              className=" rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
+              type="submit"
+            >
+              Save
+            </button>
+          </div>
         </form>
       </section>
     </>
