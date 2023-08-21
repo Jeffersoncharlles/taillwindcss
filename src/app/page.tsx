26864b2c558e9ac3821b02c09ccb85008a35aaff
Jefferson Charlles <contato@jefferdeveloper.com>
2023-08-21 11:30:05 -0300

    chore:varient button with tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { SettingsTabs } from '@/components/SettingsTabs'
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
 import { SelectItem } from '@/components/Form/Select/SelectItem'
 import { TextArea } from '@/components/Form/TextArea'
+import { Button } from '@/components/Button'
 
 export default function Home() {
   return (
@@ -24,19 +25,12 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-2">
-            <button
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50 dark:border-zinc-400 dark:text-zinc-300 dark:hover:bg-zinc-800"
-              type="button"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              className=" rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-              type="submit"
-              form="settings"
-            >
+            </Button>
+            <Button type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
         <form
@@ -216,18 +210,10 @@ export default function Home() {
           </div>
 
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50 dark:border-zinc-400 dark:text-zinc-300 dark:hover:bg-zinc-800"
-              type="button"
-            >
+            <Button type="button" variant="outline">
               Cancel
-            </button>
-            <button
-              className=" rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-              type="submit"
-            >
-              Save
-            </button>
+            </Button>
+            <Button type="submit">Save</Button>
           </div>
         </form>
       </section>
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,7 +1,25 @@
-interface ButtonProps {
-  title: string
-}
+import { tv, VariantProps } from 'tailwind-variants'
 
-export function Button(props: ButtonProps) {
-  return <div></div>
+const button = tv({
+  base: [
+    'rounded-lg px-4 py-2 text-sm font-semibold shadow-sm outline-none',
+    'focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-violet-500',
+    'active:opacity-80',
+  ],
+  variants: {
+    variant: {
+      primary: 'bg-violet-600  text-white  hover:bg-violet-700',
+      outline:
+        'border border-zinc-300  text-zinc-700  hover:bg-zinc-50 dark:border-zinc-400 dark:text-zinc-300 dark:hover:bg-zinc-800',
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+  },
+})
+type ButtonProps = React.ComponentProps<'button'> & VariantProps<typeof button>
+export function Button({ variant, ...rest }: ButtonProps) {
+  return <button className={button({ variant })} {...rest} />
 }
+//
